eb565796cc151eb69691709f2fbed846a9825cc9
Otari Tchokhonelidze <otchokhonelidze4@gmail.com>
2025-11-20 14:13:46 +0400

    fetch data in posts, add search field in users page

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-posts',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './posts.component.html',
-  styleUrl: './posts.component.scss'
+  styleUrl: './posts.component.scss',
 })
 export class PostsComponent {
+  posts: any[] = [];
+  userId: number | null = null;
+  selectedPost: any = null;
+  isModalOpen: boolean = false;
 
+  constructor(private http: HttpClient, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      this.userId = params['userId'] ? +params['userId'] : null;
+
+      this.http
+        .get<any[]>('https://jsonplaceholder.typicode.com/posts')
+        .subscribe((data) => {
+          this.posts = this.userId
+            ? data.filter((p) => p.userId === this.userId)
+            : data;
+        });
+    });
+  }
+
+  openPostDetail(post: any) {
+    this.selectedPost = post;
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.selectedPost = null;
+    this.isModalOpen = false;
+  }
 }
--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-users',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss',
 })
 export class UsersComponent {
   users: any[] = [];
+  searchTerm: string = '';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   ngOnInit() {
     this.http
@@ -19,5 +22,26 @@ export class UsersComponent {
       .subscribe((data) => {
         this.users = data;
       });
+  }
+
+  goToPosts(userId: number) {
+    this.router.navigate(['/posts'], { queryParams: { userId } });
+  }
+
+  filteredUsers() {
+    console.log('ok');
+    if (!this.searchTerm) return this.users;
+
+    const term = this.searchTerm.toLowerCase();
+    return this.users.filter((u) => {
+      const firstName = u.name.split(' ')[0].toLowerCase();
+      const lastName = u.name.split(' ')[1]?.toLowerCase() || '';
+      const email = u.email.toLowerCase();
+      return (
+        firstName.includes(term) ||
+        lastName.includes(term) ||
+        email.includes(term)
+      );
+    });
   }
 }
